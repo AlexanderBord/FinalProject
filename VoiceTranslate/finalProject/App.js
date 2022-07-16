e4b621621c57f3090463e0674a812535b2e67172
Alex <alexanderbord23@gmail.com>
2022-07-16 15:09:27 +0300

    test

---
+++ VoiceTranslate/finalProject/App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import RouteInitializer from './src/router/RouteInitializer';
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs()
+
+const App = () => {
+  return (<RouteInitializer />);
+}
+export default App;
+
+
+
+
+
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   section: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+// import React, { useState, useEffect } from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
