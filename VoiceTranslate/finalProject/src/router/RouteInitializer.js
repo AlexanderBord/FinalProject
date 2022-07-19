683be7a6cb6dff517ceddcc236a5eec08a0590fd
Alex Bord <74188589+AlexanderBord@users.noreply.github.com>
2022-07-19 10:28:56 +0300

    Update RouteInitializer.js

--- VoiceTranslate/finalProject/src/router/RouteInitializer.js
+++ VoiceTranslate/finalProject/src/router/RouteInitializer.js
@@ -3,12 +3,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Router from './Router';
 
-
+//From initializing navigation, is responsible for checking with the asynchronous storage the status of the user's link
+//to the system and, accordingly, to initialize the screens required to transfer it to the appropriate screen.
 const RouteInitializer = () => {
     const [initialized, setInitialized] = useState(false)
     const [initialRoute, setInitialRoute] = useState('SignIn')
 
     useEffect(() => {
+        //Achieving the user's connection status to the system and router accordingly
         async function getStorageValue() {
             let loggedIn, whatsAppAuthentication;
             try {
