d2d2ed618f9b382e3d41364545f3987dbe71220e
Alex Bord <74188589+AlexanderBord@users.noreply.github.com>
2022-07-19 10:24:57 +0300

    Update FireBase.js

--- VoiceTranslate/finalProject/src/dataBase/FireBase.js
+++ VoiceTranslate/finalProject/src/dataBase/FireBase.js
@@ -1,8 +1,12 @@
 import firestore from '@react-native-firebase/firestore';
 
+//reference to a users collection in firebase database
 const ref = firestore().collection('users');
+
+//global variable will hold the users list
 var list = [];
 
+//Function responsible for adding a user to the list of users in the database after checking if it exists in the database
 export const writeUserInDb = async (data) => {
   if (!await checkIfUserExist(data)) {
     await ref.add(data);
@@ -12,6 +16,7 @@ export const writeUserInDb = async (data) => {
   }
 };
 
+//A function is responsible for querying the database at the request of the user list
 const getUsers = async () => {
   await firestore().collection('users').get().then(querySnapshot => {
     querySnapshot.forEach(doc => {
@@ -23,8 +28,8 @@ const getUsers = async () => {
   });
 };
 
+//A function is responsible for querying the database to check the existence of the user in the database
 export const checkIfUserExist = async (data) => {
-
   await getUsers();
   for (let item of list) {
     if (data.userName === item.userName && data.password === item.password) {
